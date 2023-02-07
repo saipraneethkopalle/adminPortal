23f5a5ddf5d6dcbf85fd0849ff553123469c5169
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-02-07 12:37:20 +0530

    changes done

--- constants/Config.js
+++ constants/Config.js
@@ -7,5 +7,5 @@ exports.STATUS =
      CREATED: 201 ,
      NO_CONTENT: 204 }
 exports.DBURL_PROD ="mongodb://dbuser:Abcd1234@127.0.0.1:27017/admin"
-exports.DBURL_DEV = "mongodb://localhost:27017/testing"
+exports.DBURL_DEV = "mongodb://localhost:27017/adminPortal"
 
