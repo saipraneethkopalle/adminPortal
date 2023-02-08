e5ae274050f0e7e47252e7f6f4742b54544269a5
PavanEminence <115974082+PavanEminence@users.noreply.github.com>
2023-02-08 14:48:34 +0530

    changes done

--- models/apiWhitelist.js
+++ models/apiWhitelist.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const uniqueValidator = require("mongoose-unique-validator");
 const apiWhiteListSchema = mongoose.Schema({
-   customerName: { type: String,required:true},
-   ipAddress:{type:String,required:true},
+   customerName: { type: String,required:true,unique:true},
+   ipAddress:{type:String,required:true,unique:true},
    plan1:{type:Boolean,default:true},
    plan2:{type:Boolean,default:true},
    plan3:{type:Boolean,required:true},
