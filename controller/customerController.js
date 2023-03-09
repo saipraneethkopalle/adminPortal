4a3289bc181350b4a1ff3e96a29425e37fc63106
saipraneethkopalle <115973738+saipraneethkopalle@users.noreply.github.com>
2023-03-09 09:53:31 +0530

    changes done

--- controller/customerController.js
+++ controller/customerController.js
@@ -9,11 +9,15 @@ exports.getDFancyBM = async (req, res) => {
         let ids = (req.params.marketId).split(',')
         // console.log("m",ids);
         let dFancy = await getMultiMarkets(ids, 'Fancy-', '-diamond')
-        let data = { "Fancy": [], "BookMaker": [] }
-        for (let d of dFancy) {
-            data.Fancy.push(d.data?.t3);
-            data.BookMaker.push(d.data?.t2);
-        }
+        let data = { "Fancy": [], "BookMaker": [] }        
+	for (let d of dFancy) {
+	    if(d.data?.t3!=null && d.data.t3.length > 0){
+	    data.Fancy.push(...d.data?.t3);
+	    }
+	   if(d.data?.t2 != null && d.data.t2.length > 0){
+            data.BookMaker.push(...d.data?.t2);
+	   }
+        }	
         let result = {
             "message": "Diamond Result fetched",
             "data": data
@@ -29,16 +33,16 @@ exports.getSFancyBM = async (req, res) => {
         let ids = (req.params.marketId).split(',')  
         const data = [getMultiMarkets(ids, 'Fancy-', '-sky'),getMultiMarkets(ids, 'BM-', '-sky'),getMultiMarkets(ids, 'Odds-', '-sky')]
         const result = await Promise.allSettled(data)
-        // console.log("res",result);
-        let skyf=result[0].value.length > 0?result[0].value[0]:[]
-        skyf = skyf?.filter(resp => {
-            if (resp.status != '18' && resp.status != '1' && resp.status != '14') {
-                return resp
-            }
-        })
+        // console.log("res",result[0].value);
+        let skyf=result[0].value.length > 0?result[0].value:[]
+	//skyf[0] = skyf[0]?.filter(resp => {
+        //    if (resp.status != '18' && resp.status != '1' && resp.status != '14') {
+        //        return resp
+        //    }
+        //})
         let finalResult = {
             "message": "Sky Result fetched",
-            "data": { "Fancy": {"sky": skyf}, "BookMaker": {"sky": result[1].value.length > 0?result[1].value[0]:[]}, "Odds": { "sky":result[2].value.length > 0?result[2].value[0]:[]} }
+            "data": { "Fancy": {"sky": skyf}, "BookMaker": {"sky": result[1].value.length > 0?result[1].value:[]}, "Odds": { "sky":result[2].value.length > 0?result[2].value:[]} }
         }
         return res.status(STATUS.OK).send(finalResult)
     } catch (err) {
@@ -52,7 +56,7 @@ exports.getS3FancyBM = async (req, res) => {
         const result = await Promise.allSettled(data)
         let finalResult = {
             "message": "Sky Result fetched",
-            "data": { "Fancy": { "sky3": result[0].value.length > 0?result[0].value[0]:[] }, "BookMaker": { "sky3": result[1].value.length > 0?result[1].value[0]:[] } }
+            "data": { "Fancy": { "sky3": result[0].value.length > 0?result[0].value:[] }, "BookMaker": { "sky3": result[1].value.length > 0?result[1].value:[] } }
         }
         return res.status(STATUS.OK).send(finalResult)
     } catch (err) {
@@ -65,7 +69,7 @@ exports.getSkyFancyBM = async (req, res) => {
         let ids = (req.params.marketId).split(',')
         const data = [getMultiMarkets(ids, 'Fancy-', '-sky'), getMultiMarkets(ids, 'BM-', '-sk')]
         const result = await Promise.allSettled(data)
-	    let skyf =result[0].value.length > 0?result[0].value[0]:[]
+	    let skyf =result[0].value.length > 0?result[0].value:[]
 	    // console.log("skyf",skyf.length)
 	    skyf=skyf?.filter(resp=>{
                 if(resp.status != '18' && resp.status != '1' && resp.status != '14'){
@@ -75,7 +79,7 @@ exports.getSkyFancyBM = async (req, res) => {
         // console.log("sky",skyf.length);
         let finalResult = {
             "message": "Sky Result fetched",
-            "data": { "Fancy": skyf, "BookMaker": result[1].value.length > 0 ? result[1].value[0]:[]}
+            "data": { "Fancy": skyf, "BookMaker": result[1].value.length > 0 ? result[1].value:[]}
         }
         return res.status(STATUS.OK).send(finalResult)
     } catch (err) {
@@ -89,7 +93,7 @@ exports.getWFancyBM = async (req, res) => {
         const data =await Promise.allSettled([getMultiMarkets(ids, 'Fancy-', '-world'),getMultiMarkets(ids, 'BM-', '-world')])
         let result = {
             "message": "World Result fetched",
-            "data": { "Fancy": data[0].value, "BookMaker": data[1].value }
+            "data": { "Fancy": data[0].value.length > 0 ? data[0].value:[], "BookMaker": data[1].value.length > 0 ? data[1].value:[]}
         }
         return res.status(STATUS.OK).send(result)
     } catch (err) {
@@ -102,9 +106,10 @@ exports.getRyan = async (req, res) => {
         let ids = (req.params.marketId).split(',');
         // console.log("ids",ids,"length",ids.length);
         let ryandata = await getMultiMarkets(ids, 'Odds-', '-ryan')
+	console.log("ry",ryandata);
         let result = {
             "message": "Ryan details fetched",
-            "data": { "ryan": ryandata }
+            "data": { "ryan": ryandata.length > 0 ? ryandata:[] }
         }
         return res.status(STATUS.OK).send(result)
     } catch (err) {
@@ -118,7 +123,7 @@ exports.getTiger = async (req, res) => {
         let tigerdata = await getMultiMarkets(ids, 'Odds-', '-tiger')
         let result = {
             "message": "Tiger details fetched",
-            "data": { "tiger": tigerdata }
+            "data": { "tiger": tigerdata.length > 0 ? tigerdata:[] }
         }
         return res.status(STATUS.OK).send(result)
     } catch (err) {
@@ -131,7 +136,7 @@ exports.getBetfair = async (req, res) => {
         let betfairdata = await getMultiMarkets(ids, 'Odds-', '-betfair')
         let result = {
             "message": "Betfair details fetched",
-            "data": { "betfair": betfairdata }
+            "data": { "betfair": betfairdata.length > 0 ? betfairdata:[]}
         }
         return res.status(STATUS.OK).send(result)
     } catch (err) {
@@ -155,9 +160,29 @@ exports.getVirtualMatches = async (req, res) => {
 const getMultiMarkets = async (arr, first, last) => {
     let result = [];
     for (let i = 0; i < arr.length; i++) {
-        await new Promise((resolve) => setImmediate(async() => {
+
         let data = JSON.parse(await redisdb.GetRedis(first + arr[i] + last))
-        let getdata = data != null ? Array.isArray(data) ? result.push(data) : result.push(data) : []
+	//console.log("data",data)
+	//data=data != null && data.length > 0 ? data[0]:[]
+        //let getdata = data != null ? Array.isArray(data) ? result.push(data) : result.push(data) : []
+	let getdata = data != null ? Array.isArray(data) ? data : data : []
+	if(last == '-sky' || last=='-sk'){
+	getdata = getdata.filter(resp => {
+            if (resp.status != '18' && resp.status != '1' && resp.status != '14') {
+                return resp
+            }})
+	
+	result.push(...getdata);
+	}else{
+	if(Array.isArray(getdata) && getdata.length > 0){
+	result.push(...getdata);
+	}else{
+	if(!Array.isArray(getdata)){
+	result.push(getdata);
+	}
+	}
+	}
+    await new Promise((resolve) => setImmediate(async() => {
             resolve();
     }));            
     }
